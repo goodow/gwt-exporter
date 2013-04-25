ec856ace8bd3ff5c5d1f47dddf85855a6e073f8d
Larry Tin <i@goodow.com>
2013-04-25 23:08:18 +0800

    format

--- core/src/main/java/org/timepedia/exporter/rebind/JExportableParameter.java
+++ core/src/main/java/org/timepedia/exporter/rebind/JExportableParameter.java
@@ -8,62 +8,14 @@ import com.google.gwt.core.ext.typeinfo.JPrimitiveType;
  */
 public class JExportableParameter {
 
-  private JParameter param;
-  private JExportableMethod method;
-  private ExportableTypeOracle xTypeOracle;
+  private final JParameter param;
+  private final JExportableMethod method;
+  private final ExportableTypeOracle xTypeOracle;
 
-  public JParameter getParam() {
-    return param;
-  }
-
-
-  public JExportableClassType getExportableEnclosingType() {
-    return method.getEnclosingExportType();
-  }
-
-  public JExportableParameter(JExportableMethod exportableMethod,
-      JParameter param) {
+  public JExportableParameter(JExportableMethod exportableMethod, JParameter param) {
     this.param = param;
     this.method = exportableMethod;
     xTypeOracle = getExportableEnclosingType().getExportableTypeOracle();
-  }
-
-  public String getTypeName() {
-    return param.getType().getQualifiedSourceName();
-  }
-  
-  public String getJNISignature() {
-    return param.getType().getJNISignature();
-  }
-
-  public String getExportParameterValue(String argName) {
-    
-    String paramTypeName = param.getType().getQualifiedSourceName();
-    JExportableType type = xTypeOracle.findExportableType(paramTypeName);
-    
-    if (type != null && type.needsExport()) {
-      JExportableClassType cType = (JExportableClassType) type;
-      if (xTypeOracle.isClosure((JExportableClassType) type)) {
-        return argName + " == null ? null : (" + argName + ".constructor == $wnd."
-            + cType.getJSQualifiedExportName() + " ? " + argName
-            + "." + ClassExporter.GWT_INSTANCE + " : " 
-            + "@" + cType.getQualifiedExporterImplementationName() + "::"
-            + "makeClosure(Lcom/google/gwt/core/client/JavaScriptObject;)("
-            + argName 
-            + "))";
-      } else if (xTypeOracle.isExportable((JExportableClassType) type)) {
-        return argName + "." + ClassExporter.GWT_INSTANCE;
-      }
-    }
-    
-    if (param.getType().isClass() != null
-        && !xTypeOracle.isJavaScriptObject(param.getType())
-        && !xTypeOracle.isString(param.getType())) {
-      return "@org.timepedia.exporter.client.ExporterUtil::gwtInstance(Ljava/lang/Object;)("
-          + argName + ")";
-    }
-
-    return argName;
   }
 
   @Override
@@ -77,6 +29,44 @@ public class JExportableParameter {
 
     JExportableParameter that = (JExportableParameter) o;
     return getJsTypeOf().equals(that.getJsTypeOf());
+  }
+
+  public JExportableClassType getExportableEnclosingType() {
+    return method.getEnclosingExportType();
+  }
+
+  public JExportableType getExportableType(boolean b) {
+    return method.getExportable(param.getType(), b);
+  }
+
+  public String getExportParameterValue(String argName) {
+
+    String paramTypeName = param.getType().getQualifiedSourceName();
+    JExportableType type = xTypeOracle.findExportableType(paramTypeName);
+
+    if (type != null && type.needsExport()) {
+      JExportableClassType cType = (JExportableClassType) type;
+      if (xTypeOracle.isClosure((JExportableClassType) type)) {
+        return argName + " == null ? null : (" + argName + ".constructor == $wnd."
+            + cType.getJSQualifiedExportName() + " ? " + argName + "." + ClassExporter.GWT_INSTANCE
+            + " : " + "@" + cType.getQualifiedExporterImplementationName() + "::"
+            + "makeClosure(Lcom/google/gwt/core/client/JavaScriptObject;)(" + argName + "))";
+      } else if (xTypeOracle.isExportable((JExportableClassType) type)) {
+        return argName + "." + ClassExporter.GWT_INSTANCE;
+      }
+    }
+
+    if (param.getType().isClass() != null && !xTypeOracle.isJavaScriptObject(param.getType())
+        && !xTypeOracle.isString(param.getType())) {
+      return "@org.timepedia.exporter.client.ExporterUtil::gwtInstance(Ljava/lang/Object;)("
+          + argName + ")";
+    }
+
+    return argName;
+  }
+
+  public String getJNISignature() {
+    return param.getType().getJNISignature();
   }
 
   public String getJsTypeOf() {
@@ -102,10 +92,39 @@ public class JExportableParameter {
       return "@" + param.getType().getQualifiedSourceName() + "::class";
     }
   }
-  
-  public boolean isExportable() {
-    String js = getJsTypeOf();
-    return !js.contains("@") || getExportableType(false) != null;
+
+  public JParameter getParam() {
+    return param;
+  }
+
+  public String getToArrayFunc(String qsn, String argName) {
+    String ret = "ExporterUtil.";
+    String after = ")";
+    if (qsn.equals("java.lang.String[]")) {
+      ret += "toArrString";
+    } else if (qsn.equals("java.util.Date[]")) {
+      ret += "toArrDate";
+    } else if (qsn.equals("double[]")) {
+      ret += "toArrDouble";
+    } else if (qsn.equals("float[]")) {
+      ret += "toArrFloat";
+    } else if (qsn.equals("long[]")) {
+      ret += "toArrLong";
+    } else if (qsn.equals("int[]")) {
+      ret += "toArrInt";
+    } else if (qsn.equals("byte[]")) {
+      ret += "toArrByte";
+    } else if (qsn.equals("char[]")) {
+      ret += "toArrChar";
+    } else {
+      ret += "toArrObject";
+      after = ", new " + qsn.replace("]", "ExporterUtil.length(" + argName + ")]") + after;
+    }
+    return ret + "(" + argName + after;
+  }
+
+  public String getTypeName() {
+    return param.getType().getQualifiedSourceName();
   }
 
   @Override
@@ -113,38 +132,14 @@ public class JExportableParameter {
     return param != null ? getJsTypeOf().hashCode() : 0;
   }
 
-  public String toString() {
-    return param.getType().getSimpleSourceName();
+  public boolean isExportable() {
+    String js = getJsTypeOf();
+    return !js.contains("@") || getExportableType(false) != null;
   }
 
-  public JExportableType getExportableType(boolean b) {
-    return method.getExportable(param.getType(), b);
-  }
-  
-  public String getToArrayFunc(String qsn, String argName) {
-    String ret = "ExporterUtil.";
-    String after = ")";
-    if (qsn.equals("java.lang.String[]")) {
-      ret += "toArrString" ;
-    } else if (qsn.equals("java.util.Date[]")) {
-      ret += "toArrDate" ;
-    } else if (qsn.equals("double[]")) {
-      ret += "toArrDouble" ;
-    } else if (qsn.equals("float[]")) {
-      ret += "toArrFloat" ;
-    } else if (qsn.equals("long[]")) {
-      ret += "toArrLong" ;
-    } else if (qsn.equals("int[]")) {
-      ret += "toArrInt" ;
-    } else if (qsn.equals("byte[]")) {
-      ret += "toArrByte" ;
-    } else if (qsn.equals("char[]")) {
-      ret += "toArrChar" ;
-    } else {
-      ret += "toArrObject";
-      after = ", new " + qsn.replace("]", "ExporterUtil.length(" + argName + ")]") + after; 
-    }
-    return ret + "(" + argName + after;
+  @Override
+  public String toString() {
+    return param.getType().getSimpleSourceName();
   }
 
 }
